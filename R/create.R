4b72f534c8c4e24048129cf1620d8eef4269761c
Miles McBain <miles.mcbain@qfes.qld.gov.au>
2019-10-17 17:28:53 +1000

    document create()

--- R/create.R
+++ R/create.R
@@ -1,3 +1,15 @@
+##' Create a capsule library context to run code in
+##'
+##' Dependencies to be encapsulated are detected from files you nominate in
+##' `dep_source_paths`. Good practice would be to have a single dependencies R
+##' file that contains all library() calls - hence this makes an explicit
+##' assertion of your dependencies. This way spurious usages of pkg:: for
+##' packages not stated as dependencies will cause errors that can be caught.
+##' 
+##' @title create
+##' @param dep_source_paths files to find package dependencies in.
+##' @return nothing. Creates a capsule as a side effect.
+##' @author Miles McBain
 create <- function(dep_source_paths = "./packages.R") {
 
   callr::r(function(){
