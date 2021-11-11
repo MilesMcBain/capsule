4d473c816dbee3da17771c7f7f1c05eb38b64c89
Miles McBain <miles.mcbain@qfes.qld.gov.au>
2021-11-11 12:30:35 +1000

    dotenv handle unsetting new vars

--- R/dotenv.R
+++ R/dotenv.R
@@ -4,8 +4,17 @@ with_.env_if_available <- function(expr) {
   } else {
     message("{capsule} found a .env file. Setting environment for duration of this operation.")
     old_env <- Sys.getenv()
-    on.exit(do.call(Sys.setenv, old_env))
     dotenv::load_dot_env()
+    new_env <- Sys.getenv()
+    new_vars <- setdiff(names(new_env), names(old_env))
+    unset_list <- as.list(setNames(
+      rep("", length(new_vars)),
+      new_vars
+    ))
+    on.exit(do.call(
+      Sys.setenv,
+      c(as.list(old_env), unset_list)
+    ))
     force(expr)
   }
 }
