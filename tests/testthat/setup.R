3ea1d776c3e81d5e7ea4134d1944e78bb1e26713
Miles McBain <miles.mcbain@gmail.com>
2025-03-04 21:30:43 +1000

    intialise tests faster with renv

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -15,20 +15,29 @@ get_test_libpaths <- function() {
 old_repos <- getOption("repos")
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 withr::defer(options(repos = old_repos), teardown_env())
+cleanup <- function() {
+  unlink(temp_lib_path, recursive = TRUE)
+}
+# burn it all down
+withr::defer(cleanup(), teardown_env())
 
 # populate test library
 withr::with_libpaths(
   new = temp_lib_path,
-  code = install.packages(
-    "testrpkg",
-    repos = c(mm = "https://milesmcbain.r-universe.dev", CRAN = "https://cloud.r-project.org/"),
-    lib = temp_lib_path
-  )
+  code = {
+    withr::with_options(
+      list(
+        repos = c(mm = "https://milesmcbain.r-universe.dev", CRAN = "https://cloud.r-project.org/")
+      ),
+      # ideally would use pak here so package cache gets used on test runs
+      # but pack can't install using a library it itself it not in.
+      # TODO Could we create a lockfile to restore to the temp library?
+      renv::restore(
+        lockfile = testthat::test_path("testrpkg.lock"),
+        library = temp_lib_path,
+        confirm = FALSE
+      )
+    )
+  }
 )
 
-cleanup <- function() {
-  unlink(temp_lib_path, recursive = TRUE)
-}
-
-# burn it all down
-withr::defer(cleanup(), teardown_env())
